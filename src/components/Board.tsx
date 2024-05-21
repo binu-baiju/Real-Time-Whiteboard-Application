b33dda4dad9550294681c776ea189e903dd10a52
Binu Baiju <binub_mca2107022@ahead.students.amrita.edu>
2024-05-21 18:19:36 +0530

    "done redo and undo"

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -9,6 +9,8 @@ const Board = () => {
 
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const shouldDraw = useRef(false);
+  const drawHistory = useRef<ImageData[]>([]);
+  const historyPointer = useRef(0);
   const { activeMenuItem, actionMenuItem } = useSelector(
     (state: RootState) => state.menu
   );
@@ -21,15 +23,31 @@ const Board = () => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
 
+    if (!context) return;
+
     if (actionMenuItem === MENU_ITEMS.DOWNLOAD) {
       const URL = canvas.toDataURL();
       const anchor = document.createElement("a");
       anchor.href = URL;
       anchor.download = "sketch.jpg";
       anchor.click();
+    } else if (
+      actionMenuItem === MENU_ITEMS.UNDO ||
+      actionMenuItem === MENU_ITEMS.REDO
+    ) {
+      if (historyPointer.current > 0 && actionMenuItem === MENU_ITEMS.UNDO)
+        historyPointer.current -= 1;
+      if (
+        historyPointer.current < drawHistory.current.length - 1 &&
+        actionMenuItem === MENU_ITEMS.REDO
+      )
+        historyPointer.current += 1;
+      const imageData = drawHistory.current[historyPointer.current];
+      context.putImageData(imageData, 0, 0);
     }
+
     dispatch(actionItemClick(null));
-  }, [actionMenuItem]);
+  }, [actionMenuItem, dispatch]);
 
   useEffect(() => {
     if (!canvasRef.current) return;
@@ -80,6 +98,9 @@ const Board = () => {
       console.log("mouseDown");
 
       shouldDraw.current = false;
+      const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+      drawHistory.current.push(imageData);
+      historyPointer.current = drawHistory.current.length - 1;
     };
 
     canvas.addEventListener("mousedown", handleMouseDown);
